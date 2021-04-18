9319bbba409920b1cfc2e672bd7735fa4811026c
Wilman Ortiz <willman.ortiz@gmail.com>
2021-04-18 14:38:49 -0500

    feat: Define proyecto del servidor gateway

---
+++ src/test/java/co/com/demo/cloud/servergateway/ServerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.demo.cloud.servergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
